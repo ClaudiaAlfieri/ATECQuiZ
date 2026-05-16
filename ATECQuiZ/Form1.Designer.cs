95ffa89887dec617ea68867fccc34c237d25a395
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-17 00:07:42 +0100

    Create new buttons

--- ATECQuiZ/Form1.Designer.cs
+++ ATECQuiZ/Form1.Designer.cs
@@ -29,60 +29,14 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.btnNovoJogo = new System.Windows.Forms.Button();
-            this.btnHighscores = new System.Windows.Forms.Button();
-            this.btnSair = new System.Windows.Forms.Button();
             this.btnCinema = new System.Windows.Forms.Button();
             this.btnHistoria = new System.Windows.Forms.Button();
             this.btnMix = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.btnNovoJogo = new System.Windows.Forms.Button();
+            this.btnHighscores = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnNovoJogo
-            // 
-            this.btnNovoJogo.BackColor = System.Drawing.Color.Magenta;
-            this.btnNovoJogo.FlatAppearance.BorderColor = System.Drawing.Color.Magenta;
-            this.btnNovoJogo.FlatAppearance.BorderSize = 2;
-            this.btnNovoJogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNovoJogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNovoJogo.ForeColor = System.Drawing.Color.White;
-            this.btnNovoJogo.Location = new System.Drawing.Point(557, 369);
-            this.btnNovoJogo.Name = "btnNovoJogo";
-            this.btnNovoJogo.Size = new System.Drawing.Size(200, 50);
-            this.btnNovoJogo.TabIndex = 0;
-            this.btnNovoJogo.Text = "🎮 Novo Jogo";
-            this.btnNovoJogo.UseVisualStyleBackColor = false;
-            // 
-            // btnHighscores
-            // 
-            this.btnHighscores.BackColor = System.Drawing.Color.DarkOrchid;
-            this.btnHighscores.FlatAppearance.BorderColor = System.Drawing.Color.DarkOrchid;
-            this.btnHighscores.FlatAppearance.BorderSize = 2;
-            this.btnHighscores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHighscores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHighscores.ForeColor = System.Drawing.Color.White;
-            this.btnHighscores.Location = new System.Drawing.Point(557, 449);
-            this.btnHighscores.Name = "btnHighscores";
-            this.btnHighscores.Size = new System.Drawing.Size(200, 50);
-            this.btnHighscores.TabIndex = 1;
-            this.btnHighscores.Text = " 🏆 Highscores";
-            this.btnHighscores.UseVisualStyleBackColor = false;
-            // 
-            // btnSair
-            // 
-            this.btnSair.BackColor = System.Drawing.Color.MediumSlateBlue;
-            this.btnSair.FlatAppearance.BorderColor = System.Drawing.Color.MediumSlateBlue;
-            this.btnSair.FlatAppearance.BorderSize = 2;
-            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSair.ForeColor = System.Drawing.Color.White;
-            this.btnSair.Location = new System.Drawing.Point(557, 526);
-            this.btnSair.Name = "btnSair";
-            this.btnSair.Size = new System.Drawing.Size(200, 50);
-            this.btnSair.TabIndex = 2;
-            this.btnSair.Text = "❌ Sair";
-            this.btnSair.UseVisualStyleBackColor = false;
-            this.btnSair.Click += new System.EventHandler(this.btnSair_Click_1);
             // 
             // btnCinema
             // 
@@ -145,6 +99,52 @@
             this.button1.TabIndex = 6;
             this.button1.UseVisualStyleBackColor = false;
             // 
+            // btnNovoJogo
+            // 
+            this.btnNovoJogo.BackColor = System.Drawing.Color.Transparent;
+            this.btnNovoJogo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnNovoJogo.BackgroundImage")));
+            this.btnNovoJogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnNovoJogo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNovoJogo.FlatAppearance.BorderSize = 0;
+            this.btnNovoJogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNovoJogo.ForeColor = System.Drawing.Color.Transparent;
+            this.btnNovoJogo.Location = new System.Drawing.Point(588, 369);
+            this.btnNovoJogo.Name = "btnNovoJogo";
+            this.btnNovoJogo.Size = new System.Drawing.Size(158, 64);
+            this.btnNovoJogo.TabIndex = 7;
+            this.btnNovoJogo.UseVisualStyleBackColor = false;
+            // 
+            // btnHighscores
+            // 
+            this.btnHighscores.BackColor = System.Drawing.Color.Transparent;
+            this.btnHighscores.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnHighscores.BackgroundImage")));
+            this.btnHighscores.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnHighscores.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHighscores.FlatAppearance.BorderSize = 0;
+            this.btnHighscores.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHighscores.ForeColor = System.Drawing.Color.Transparent;
+            this.btnHighscores.Location = new System.Drawing.Point(588, 451);
+            this.btnHighscores.Name = "btnHighscores";
+            this.btnHighscores.Size = new System.Drawing.Size(158, 64);
+            this.btnHighscores.TabIndex = 8;
+            this.btnHighscores.UseVisualStyleBackColor = false;
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Transparent;
+            this.btnSair.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSair.BackgroundImage")));
+            this.btnSair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSair.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.ForeColor = System.Drawing.Color.Transparent;
+            this.btnSair.Location = new System.Drawing.Point(588, 538);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(158, 58);
+            this.btnSair.TabIndex = 9;
+            this.btnSair.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -152,13 +152,13 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1305, 681);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnHighscores);
+            this.Controls.Add(this.btnNovoJogo);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnMix);
             this.Controls.Add(this.btnHistoria);
             this.Controls.Add(this.btnCinema);
-            this.Controls.Add(this.btnSair);
-            this.Controls.Add(this.btnHighscores);
-            this.Controls.Add(this.btnNovoJogo);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -166,14 +166,13 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnNovoJogo;
-        private System.Windows.Forms.Button btnHighscores;
-        private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button btnCinema;
         private System.Windows.Forms.Button btnHistoria;
         private System.Windows.Forms.Button btnMix;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnNovoJogo;
+        private System.Windows.Forms.Button btnHighscores;
+        private System.Windows.Forms.Button btnSair;
     }
 }
 
