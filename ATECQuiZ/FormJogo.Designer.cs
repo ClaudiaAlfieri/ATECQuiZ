42534ae4538110fdc2593817f9e6b9c92f16f3bb
ClaudiaAlfieri <claudia.jorge86@gmail.com>
2026-05-19 22:01:53 +0100

    Create VerificarResposta()

--- ATECQuiZ/FormJogo.Designer.cs
+++ ATECQuiZ/FormJogo.Designer.cs
@@ -36,13 +36,14 @@
             this.btnResposta2 = new System.Windows.Forms.Button();
             this.btnResposta3 = new System.Windows.Forms.Button();
             this.btnResposta4 = new System.Windows.Forms.Button();
+            this.lblNivel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblTema
             // 
             this.lblTema.AutoSize = true;
             this.lblTema.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTema.Location = new System.Drawing.Point(324, 119);
+            this.lblTema.Location = new System.Drawing.Point(234, 119);
             this.lblTema.Name = "lblTema";
             this.lblTema.Size = new System.Drawing.Size(59, 24);
             this.lblTema.TabIndex = 0;
@@ -52,7 +53,7 @@
             // 
             this.lblPerguntaNum.AutoSize = true;
             this.lblPerguntaNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPerguntaNum.Location = new System.Drawing.Point(465, 119);
+            this.lblPerguntaNum.Location = new System.Drawing.Point(566, 119);
             this.lblPerguntaNum.Name = "lblPerguntaNum";
             this.lblPerguntaNum.Size = new System.Drawing.Size(160, 24);
             this.lblPerguntaNum.TabIndex = 1;
@@ -62,7 +63,7 @@
             // 
             this.lblPontuacao.AutoSize = true;
             this.lblPontuacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPontuacao.Location = new System.Drawing.Point(695, 119);
+            this.lblPontuacao.Location = new System.Drawing.Point(849, 110);
             this.lblPontuacao.Name = "lblPontuacao";
             this.lblPontuacao.Size = new System.Drawing.Size(100, 24);
             this.lblPontuacao.TabIndex = 2;
@@ -72,7 +73,7 @@
             // 
             this.lblPergunta.AutoSize = true;
             this.lblPergunta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPergunta.Location = new System.Drawing.Point(494, 235);
+            this.lblPergunta.Location = new System.Drawing.Point(408, 231);
             this.lblPergunta.Name = "lblPergunta";
             this.lblPergunta.Size = new System.Drawing.Size(86, 24);
             this.lblPergunta.TabIndex = 3;
@@ -81,42 +82,56 @@
             // btnResposta1
             // 
             this.btnResposta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnResposta1.Location = new System.Drawing.Point(240, 335);
+            this.btnResposta1.Location = new System.Drawing.Point(133, 335);
             this.btnResposta1.Name = "btnResposta1";
-            this.btnResposta1.Size = new System.Drawing.Size(120, 35);
+            this.btnResposta1.Size = new System.Drawing.Size(250, 35);
             this.btnResposta1.TabIndex = 4;
             this.btnResposta1.Text = "Resposta 1";
             this.btnResposta1.UseVisualStyleBackColor = true;
+            this.btnResposta1.Click += new System.EventHandler(this.btnResposta1_Click);
             // 
             // btnResposta2
             // 
             this.btnResposta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnResposta2.Location = new System.Drawing.Point(412, 335);
             this.btnResposta2.Name = "btnResposta2";
-            this.btnResposta2.Size = new System.Drawing.Size(120, 35);
+            this.btnResposta2.Size = new System.Drawing.Size(250, 35);
             this.btnResposta2.TabIndex = 5;
             this.btnResposta2.Text = "Resposta 2";
             this.btnResposta2.UseVisualStyleBackColor = true;
+            this.btnResposta2.Click += new System.EventHandler(this.btnResposta2_Click);
             // 
             // btnResposta3
             // 
             this.btnResposta3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnResposta3.Location = new System.Drawing.Point(601, 335);
+            this.btnResposta3.Location = new System.Drawing.Point(699, 335);
             this.btnResposta3.Name = "btnResposta3";
-            this.btnResposta3.Size = new System.Drawing.Size(120, 35);
+            this.btnResposta3.Size = new System.Drawing.Size(250, 35);
             this.btnResposta3.TabIndex = 6;
             this.btnResposta3.Text = "Resposta 3";
             this.btnResposta3.UseVisualStyleBackColor = true;
+            this.btnResposta3.Click += new System.EventHandler(this.btnResposta3_Click);
             // 
             // btnResposta4
             // 
             this.btnResposta4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnResposta4.Location = new System.Drawing.Point(765, 335);
+            this.btnResposta4.Location = new System.Drawing.Point(975, 335);
             this.btnResposta4.Name = "btnResposta4";
-            this.btnResposta4.Size = new System.Drawing.Size(120, 35);
+            this.btnResposta4.Size = new System.Drawing.Size(250, 35);
             this.btnResposta4.TabIndex = 7;
             this.btnResposta4.Text = "Resposta 4";
             this.btnResposta4.UseVisualStyleBackColor = true;
+            this.btnResposta4.Click += new System.EventHandler(this.btnResposta4_Click);
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNivel.Location = new System.Drawing.Point(393, 119);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(52, 24);
+            this.lblNivel.TabIndex = 8;
+            this.lblNivel.Text = "Nível";
             // 
             // FormJogo
             // 
@@ -124,6 +139,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1280, 647);
+            this.Controls.Add(this.lblNivel);
             this.Controls.Add(this.btnResposta4);
             this.Controls.Add(this.btnResposta3);
             this.Controls.Add(this.btnResposta2);
@@ -150,5 +166,6 @@
         private System.Windows.Forms.Button btnResposta2;
         private System.Windows.Forms.Button btnResposta3;
         private System.Windows.Forms.Button btnResposta4;
+        private System.Windows.Forms.Label lblNivel;
     }
 }
--- ATECQuiZ/FormJogo.cs
+++ ATECQuiZ/FormJogo.cs
@@ -13,6 +13,7 @@ namespace ATECQuizApp
         int nivelAtual = 1;
         int perguntaAtual = 0;
         int pontuacao = 0;
+        int certasNesteNivel = 0;
 
         public FormJogo(string tema)
         {
@@ -47,6 +48,26 @@ namespace ATECQuizApp
             }
 
             perguntaAtual = 0;
+            BaralharPergunta();
+        }
+
+        private void BaralharPergunta()
+        {
+            Random rnd = new Random();
+            for (int i = perguntasDoJogo.Count - 1; i > 0; i--)
+            {
+                int j = rnd.Next(0, i + 1);
+
+                XmlNode temp = perguntasDoJogo[i];
+                perguntasDoJogo[i] = perguntasDoJogo[j];
+                perguntasDoJogo[j] = temp;
+            }
+
+            if (perguntasDoJogo.Count > 5)
+            {
+                perguntasDoJogo = perguntasDoJogo.GetRange(0, 5);
+            }
+            
             MostrarPergunta();
         }
 
@@ -54,7 +75,8 @@ namespace ATECQuizApp
         {
             XmlNode node = perguntasDoJogo[perguntaAtual];
 
-            lblTema.Text = temaEscolhido;
+            lblTema.Text = "Tema: " + temaEscolhido;
+            lblNivel.Text = "Nível: " + nivelAtual.ToString();
             lblPerguntaNum.Text = "Pergunta: " + (perguntaAtual + 1);
             lblPontuacao.Text = "Pontuação: " + pontuacao;
             lblPergunta.Text = node["Text"].InnerText;
@@ -67,6 +89,70 @@ namespace ATECQuizApp
             btnResposta4.Text = opcoes[3].InnerText;
         }
 
+        private void VerificarResposta(string respostaEscolhida)
+        {
+            string respostaCorreta = perguntasDoJogo[perguntaAtual]["CorrectAnswer"].InnerText;
+           
+            if (respostaEscolhida == respostaCorreta)
+            {
+                MessageBox.Show("Resposta correta! 🎉", "Resultado");
+                pontuacao++;
+                certasNesteNivel += 1;
+            }
+            else
+            {
+                MessageBox.Show("Resposta errada! 😔", "Resultado");
+            }
 
+            perguntaAtual++;
+
+            if (perguntaAtual < 5)
+            {
+                MostrarPergunta();
+            }
+            else
+            {
+               if (certasNesteNivel < 4)
+                {
+                    MessageBox.Show("Jogo encerrado");
+                    this.Close();
+                }
+                else
+                {
+                    if (nivelAtual == 3)
+                    {
+                        MessageBox.Show("Parabéns, venceste o jogo! 🏆");
+                        this.Close();
+                    }
+                    else
+                    {
+                        nivelAtual++;
+                        certasNesteNivel = 0;
+                        CarregarPerguntas();
+                    }
+                }
+            }
+
+        }
+
+        private void btnResposta1_Click(object sender, EventArgs e)
+        {
+            VerificarResposta(btnResposta1.Text);
+        }
+
+        private void btnResposta2_Click(object sender, EventArgs e)
+        {
+            VerificarResposta(btnResposta2.Text);
+        }
+
+        private void btnResposta3_Click(object sender, EventArgs e)
+        {
+            VerificarResposta(btnResposta3.Text);
+        }
+
+        private void btnResposta4_Click(object sender, EventArgs e)
+        {
+            VerificarResposta(btnResposta4.Text);
+        }
     }
 }
